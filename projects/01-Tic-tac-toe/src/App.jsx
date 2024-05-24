b47e72878c497bcbe8cd30684a68c646ee3e18e7
Alex Martínez <martinez246alex@gmail.com>
2024-05-23 23:50:07 -0500

    Generación de cuadrillas y corrección de de renderización

---
+++ projects/01-Tic-tac-toe/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${ isSelected ? 'selected': ''}`
+  const handleClick=()=>{
+    updateBoard(index)
+  }
+  return (<div onClick={handleClick} className={className}>{children}</div>);
+};
+
+
+
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurno] = useState(TURNS.X);
+
+  const updateBoard=()=>{
+
+  }
+
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return <Square
+          key={index}
+          index={index}
+          updateBoard={updateBoard}
+          > {board[index]}</Square>;
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.O}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.X}</Square>
+      </section>
+    </main>
+  );
+}
+
+export default App;
