28a96596f71f3441cf58315d101dd1a94a0a63f2
Alex Martínez <martinez246alex@gmail.com>
2024-05-26 07:57:24 -0500

    Agregado de eslinter como dependencia de proyecto

--- projects/01-Tic-tac-toe/src/App.jsx
+++ projects/01-Tic-tac-toe/src/App.jsx
@@ -1,65 +1,75 @@
-import { useState } from "react";
-import confetti from "canvas-confetti";
-import { Square } from "./components/Square";
-import { TURNS } from "./constants";
-import { checkWinnerFrom,checkEndGame } from "./logic/board";
-import { WinnerModal } from "./components/WinnerModal";
-function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinnerFrom, checkEndGame } from './logic/board'
+import { WinnerModal } from './components/WinnerModal'
+function App () {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null)
+  })
 
-  const [turn, setTurn] = useState(TURNS.X);
-  const [winner, setWinner] = useState(null);
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
 
- 
+  const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null));
-    setTurn(TURNS.X);
-    setWinner(null);
-  };
-
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
+  }
 
   const updateBoard = (index) => {
-    if (board[index] || winner) return;
+    if (board[index] || winner) return
 
-    const newBoard = [...board];
-    newBoard[index] = turn;
-    setBoard(newBoard);
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
 
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
-    setTurn(newTurn);
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', newTurn)
 
-    const newWinner = checkWinnerFrom(newBoard);
+    const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
       confetti()
-      setWinner(newWinner);
+      setWinner(newWinner)
     } else if (checkEndGame(newBoard)) {
-      setWinner(false);
+      setWinner(false)
     }
-  };
+  }
 
   return (
-    <main className="board">
+    <main className='board'>
       <h1>Tic tac toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
-      <section className="game">
+      <section className='game'>
         {board.map((square, index) => {
           return (
             <Square key={index} index={index} updateBoard={updateBoard}>
               {square}
             </Square>
-          );
+          )
         })}
       </section>
 
-      <section className="turn">
+      <section className='turn'>
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
-      <WinnerModal resetGame={resetGame} winner={winner}></WinnerModal>
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
--- projects/01-Tic-tac-toe/src/logic/board.js
+++ projects/01-Tic-tac-toe/src/logic/board.js
@@ -1,19 +1,18 @@
- import { WINNER_COMBOS } from "../constants";
- export const checkWinnerFrom= (boardToCheck) => {
-    for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo;
-      if (
-        boardToCheck[a] &&
+import { WINNER_COMBOS } from '../constants'
+export const checkWinnerFrom = (boardToCheck) => {
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] &&
         boardToCheck[a] === boardToCheck[b] &&
         boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a];
-      }
+    ) {
+      return boardToCheck[a]
     }
-    return null;
-  };
+  }
+  return null
+}
 
-  
- export const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square !== null);
-  };
+export const checkEndGame = (newBoard) => {
+  return newBoard.every((square) => square !== null)
+}
